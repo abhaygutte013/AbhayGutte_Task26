ec520c3d6689224d136986a54c01e9ce2b22e42a
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-10 19:57:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
